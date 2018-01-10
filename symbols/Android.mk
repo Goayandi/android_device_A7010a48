46477cc32c9e1a96b6e95da47dd3a3d254dda38d
DidntRead <gabro2003@gmail.com>
2018-01-10 15:01:46 +0200

    shims: put them in vendor

--- symbols/Android.mk
+++ symbols/Android.mk
@@ -6,4 +6,5 @@ LOCAL_SHARED_LIBRARIES := libui libgui
 LOCAL_C_INCLUDES := frameworks/native/include
 LOCAL_MODULE := libmtkshim_gui
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
