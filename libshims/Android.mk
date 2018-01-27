6dbcbdca9f58ab41cf93e4e24668902101c5f259
DidntRead <25854753+DidntRead@users.noreply.github.com>
2018-01-28 01:07:03 +0200

    update shim library

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -3,9 +3,13 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES :=  \
 	ui.cpp
-LOCAL_SHARED_LIBRARIES := libui
+LOCAL_SHARED_LIBRARIES := \
+	libui \
+	libgui
 LOCAL_MODULE := libmtk_ui
-LOCAL_C_INCLUDES += framework/native/include
+LOCAL_C_INCLUDES += \
+	frameworks/native/include \
+	frameworks/av/media/libstagefright/include
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
@@ -24,9 +28,8 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES :=  \
 	xlog.cpp
 LOCAL_SHARED_LIBRARIES := \
-	liblog \
-	libvcodecdrv
-LOCAL_MODULE := libmtk_xlogs
+	liblog
+LOCAL_MODULE := libmtk_xlog
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_PROPRIETARY_MODULE := true
--- libshims/ui.cpp
+++ libshims/ui.cpp
@@ -1,6 +1,8 @@
 #include <string>
 
 #include <ui/GraphicBuffer.h>
+#include <media/stagefright/MediaBuffer.h>
+#include <gui/BufferItemConsumer.h>
 
 extern "C" void _ZN7android13GraphicBufferC1EjjijjjP13native_handleb(
         const native_handle_t* handle,
@@ -31,11 +33,90 @@ extern "C" void _ZN7android13GraphicBufferC1EjjijjP13native_handleb(
 }
 
 
+// GraphicBuffer(uint32_t inWidth, uint32_t inHeight, PixelFormat inFormat,
+//               uint32_t inUsage, std::string requestorName = "<Unknown>");
 extern "C" void _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(
-            void *(pthis), uint32_t inWidth, uint32_t inHeight, int inFormat,
-            uint32_t inUsage, std::string requestorName);
+    uint32_t inWidth, uint32_t inHeight, int inFormat, uint32_t inUsage,
+    std::string requestorName);
 
-extern "C" void _ZN7android13GraphicBufferC1Ejjij(void *(pthis), uint32_t inWidth, uint32_t inHeight, int inFormat, uint32_t inUsage) {
-        _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(
-            pthis, inWidth, inHeight, inFormat, inUsage, "<Unknown>");
+//libcam_utils.so
+extern "C" void _ZN7android13GraphicBufferC1Ejjij(
+    uint32_t inWidth, uint32_t inHeight, int inFormat, uint32_t inUsage) {
+  std::string requestorName = "<Unknown>";
+  _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(
+      inWidth, inHeight, inFormat, inUsage, requestorName);
 }
+
+#if defined(__LP64__)
+extern "C" void _ZN7android13GraphicBufferC1EPK13native_handleNS0_16HandleWrapMethodEjjijmj(
+	void *self, const native_handle_t* handle,
+        android::GraphicBuffer::HandleWrapMethod method,
+        uint32_t width, uint32_t height, android::PixelFormat format,
+        uint32_t layerCount, uint64_t usage, uint32_t stride);
+
+extern "C" void _ZN7android13GraphicBufferC1EP19ANativeWindowBufferb(
+	void *self, ANativeWindowBuffer* buffer, bool keepOwnership)
+{
+    _ZN7android13GraphicBufferC1EPK13native_handleNS0_16HandleWrapMethodEjjijmj(
+	self, buffer->handle,
+	keepOwnership ? android::GraphicBuffer::TAKE_HANDLE : android::GraphicBuffer::WRAP_HANDLE,
+	buffer->width, buffer->height, buffer->format, (uint32_t)buffer->layerCount,
+	buffer->usage, buffer->stride);
+}
+#else
+extern "C" void _ZN7android13GraphicBufferC1EPK13native_handleNS0_16HandleWrapMethodEjjijyj(
+	void *self, const native_handle_t* handle,
+        android::GraphicBuffer::HandleWrapMethod method,
+        uint32_t width, uint32_t height, android::PixelFormat format,
+        uint32_t layerCount, uint64_t usage, uint32_t stride);
+
+extern "C" void _ZN7android13GraphicBufferC1EP19ANativeWindowBufferb(
+	void *self, ANativeWindowBuffer* buffer, bool keepOwnership)
+{
+    _ZN7android13GraphicBufferC1EPK13native_handleNS0_16HandleWrapMethodEjjijyj(
+	self, buffer->handle,
+	keepOwnership ? android::GraphicBuffer::TAKE_HANDLE : android::GraphicBuffer::WRAP_HANDLE,
+	buffer->width, buffer->height, buffer->format, buffer->layerCount,
+	buffer->usage, buffer->stride);
+}
+#endif
+
+extern "C" void _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEEb(
+	void* outProducer, void* outConsumer, bool consumerIsSurfaceFlinger);
+
+extern "C" void _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEERKNS1_INS_19IGraphicBufferAllocEEE(
+	void* outProducer, void* outConsumer, void* allocator __unused, bool consumerIsSurfaceFlinger)
+{
+    _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEEb(
+	outProducer, outConsumer, consumerIsSurfaceFlinger);
+}
+
+#if defined(__LP64__)
+extern "C" void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEmib(
+        const android::sp<android::IGraphicBufferConsumer>& consumer, uint64_t consumerUsage,
+        int bufferCount, bool controlledByApp);
+
+/* BufferItemConsumer::BufferItemConsumer(
+        const sp<IGraphicBufferConsumer>& consumer, uint32_t consumerUsage,
+        int bufferCount, bool controlledByApp) */
+extern "C" void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEjib(
+        const android::sp<android::IGraphicBufferConsumer>& consumer, uint32_t consumerUsage,
+        int bufferCount, bool controlledByApp)
+{
+     _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEmib(consumer, (uint64_t)consumerUsage, bufferCount, controlledByApp);
+}
+#else
+extern "C" void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEyib(
+        const android::sp<android::IGraphicBufferConsumer>& consumer, uint64_t consumerUsage,
+        int bufferCount, bool controlledByApp);
+
+/* BufferItemConsumer::BufferItemConsumer(
+        const sp<IGraphicBufferConsumer>& consumer, uint32_t consumerUsage,
+        int bufferCount, bool controlledByApp) */
+extern "C" void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEjib(
+        const android::sp<android::IGraphicBufferConsumer>& consumer, uint32_t consumerUsage,
+        int bufferCount, bool controlledByApp)
+{
+     _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEyib(consumer, (uint64_t)consumerUsage, bufferCount, controlledByApp);
+}
+#endif
